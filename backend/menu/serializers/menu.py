5c52bc18780c1a731b80e2b5216cf9c7512a9c71
shashvatpatel15 <shashvatpatel15@gmail.com>
2026-06-27 19:32:26 +0530

    fix: default menu items to available on creation

--- backend/menu/serializers/menu.py
+++ backend/menu/serializers/menu.py
@@ -1,7 +1,10 @@
+from rest_framework import serializers
 from masters.serializers.base import CloudinaryModelSerializer
 from menu.models.menu import Menu
 
 class MenuSerializer(CloudinaryModelSerializer):
+    is_available = serializers.BooleanField(default=True, required=False)
+
     class Meta:
         model = Menu
         fields = "__all__"
